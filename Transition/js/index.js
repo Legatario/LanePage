96471d596a25a00b542ed6872223fb5defe46b4c
Luis Phillipe <l.phillipe.d@gmail.com>
2022-09-07 15:11:45 -0300

    scheme darck implementado

--- Transition/js/index.js
+++ Transition/js/index.js
@@ -6,3 +6,19 @@ function toggleMenu(){
 }
 
 btnMenu.addEventListener('click', toggleMenu);
+
+function getNotification(){
+    if(!("Notification" in window)){
+        console.log('Navegador não suporta notificações')
+    }else if(Notification.permission === "granted"){
+        const notify = new Notification('Olá agora você sabera tudo sobre a TDX');
+        console.log('1')
+    }else if(Notification.permission !== "denied"){
+        Notification.requestPermission(function(permission){
+            if(permission === "granted"){
+                const notify = new Notification('Olá, bem vindo de volta!');
+                
+            }
+        })
+    }
+}
